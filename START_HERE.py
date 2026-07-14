9748a0d2c47a2436e7df72c2295778ce80f94d7e
Sprit158 <dac1.collier@gmail.com>
2026-07-14 01:22:00 +0100

    Set startup version to 1.7.10

--- START_HERE.py
+++ START_HERE.py
@@ -1,5 +1,5 @@
 """
-START HERE - WCL Reclear Tracker v1.7.9
+START HERE - WCL Reclear Tracker v1.7.10
 
 Normal run:
     python START_HERE.py
@@ -24,5 +24,5 @@ from main import main
 
 
 if __name__ == "__main__":
-    print("Starting WCL Reclear Tracker v1.7.9...")
+    print("Starting WCL Reclear Tracker v1.7.10...")
     main()
